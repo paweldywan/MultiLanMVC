cb6b6ac9ca624af6a52ed3a6f6a5d7ef109abb85
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-08-06 00:19:07 +0200

    Add project files.

---
+++ MultiLanMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using MultiLanMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace MultiLanMVC.Controllers
+{
+    public class HomeController : MyController
+    {
+        // GET: Home    
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Registration r)
+        {
+            return View(r);
+        }
+
+        public ActionResult ChangeLanguage(string lang)
+        {
+            new LanguageMang().SetLanguage(lang);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ MultiLanMVC/MyController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiLanMVC
+{
+    public class MyController : Controller
+    {
+        protected override IAsyncResult BeginExecuteCore(AsyncCallback callback, object state)
+        {
+            HttpCookie langCookie = Request.Cookies["culture"];
+
+            string lang;
+
+            if (langCookie != null)
+            {
+                lang = langCookie.Value;
+            }
+            else
+            {
+                var userLanguage = Request.UserLanguages;
+                var userLang = userLanguage != null ? userLanguage[0] : "";
+
+                if (userLang != "")
+                {
+                    lang = userLang;
+                }
+                else
+                {
+                    lang = LanguageMang.GetDefaultLanguage();
+                }
+            }
+
+            new LanguageMang().SetLanguage(lang);
+
+            return base.BeginExecuteCore(callback, state);
+        }
+    }
+}
